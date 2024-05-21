44da48a435c4c926f99b688bc83b10ecf5728533
Sebastian Zarate <zaratesanmiguel2019@outlook.com>
2024-05-21 15:00:20 -0500

    Initial Commit

---
+++ parcial_final/README.md
@@ -0,0 +1,3 @@
+# parcial_final
+
+A new Flutter project.
---
+++ parcial_final/android/app/src/main/kotlin/com/example/parcial_final/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.parcial_final
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
