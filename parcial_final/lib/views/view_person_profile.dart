51bad1f46c51db8d7bcac334a5db90c6f320545c
Sebastian Zarate <zaratesanmiguel2019@outlook.com>
2024-05-22 00:52:24 -0500

    Send Message Modal

--- parcial_final/lib/views/view_person_profile.dart
+++ parcial_final/lib/views/view_person_profile.dart
@@ -3,8 +3,61 @@ import 'package:parcial_final/models/person.dart';
 
 class ViewPersonProfile extends StatelessWidget {
   final Person person;
+  final TextEditingController title = TextEditingController();
+  final TextEditingController body = TextEditingController();
 
-  const ViewPersonProfile({required this.person, super.key});
+  ViewPersonProfile({required this.person, super.key});
+
+  void showMessageModal(context) {
+    showModalBottomSheet(
+        context: context,
+        isScrollControlled: true,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(20), topLeft: Radius.circular(20))),
+        builder: ((context) {
+          return 
+          Padding(
+            padding: EdgeInsets.only(
+                        bottom: MediaQuery.of(context).viewInsets.bottom),
+            child: SizedBox(
+              height: 380,
+              child: Center(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: TextField(
+                      controller: title,
+                      autofocus: true,
+                      decoration: InputDecoration(
+                          label: Text(
+                            "Message Title",
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                          border: const OutlineInputBorder()),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: TextField(
+                      controller: title,
+                      decoration: InputDecoration(
+                          label: Text(
+                            "Message Body",
+                            style: Theme.of(context).textTheme.bodyMedium,
+                          ),
+                          border: const OutlineInputBorder()),
+                    ),
+                  ),
+                  ElevatedButton(onPressed: (){ Navigator.pop(context); }, child: const Text("Send"))
+                ],
+              )),
+            ),
+          );
+        }));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +87,11 @@ class ViewPersonProfile extends StatelessWidget {
             Text(
               person.role,
             ),
-            ElevatedButton(onPressed: () {}, child: const Text("New Message"))
+            ElevatedButton(
+                onPressed: () {
+                  showMessageModal(context);
+                },
+                child: const Text("New Message"))
           ]),
         ),
       ),
